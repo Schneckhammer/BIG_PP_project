08822d74c16dfc37474843393aa82c0fa5610261
apaulau <loody0196@gmail.com>
2020-05-25 16:31:00 +0200

    working HW3 with timing

--- HW2/BydloKoder/CHEATING/exact.py
+++ HW2/BydloKoder/CHEATING/exact.py
@@ -13,10 +13,11 @@ count = 0
 data_to_plot = [[0 for j in range(1033)] for i in range(1033)]
 for i in range(len(data_right)):
     point = data_right[i]
-    data_to_plot[int(point/1033)][int(point%1033)] = 1 
+    data_to_plot[int(point/1033)][int(point%1033)] = 1
     count += 1
 
 
+print(count)
 plt.imshow(data_to_plot)
 plt.show()
 
